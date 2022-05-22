24990a1792994b0e7294edd0b783b8c306d578c4
Pedro Pataro <phsilvino.7789@hotmail.com>
2022-05-22 20:46:06 -0300

    pagina-descarte

--- Recicool/screens/DescarteScreen.tsx
+++ Recicool/screens/DescarteScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, TextInput } from "react-native";
 import { StyleSheet } from "react-native";
 import { Text, View } from "../components/Themed";
 
@@ -7,7 +7,36 @@ export default function DescarteScreen() {
   return (
     <View style={styles.container}>
       <ScrollView>
-        <Text>Descarte</Text>
+        <View style={styles.titleConteiner}>
+          <Text style={styles.title}>VIDRO</Text>
+        </View>
+        <View style={styles.contentContainer}>
+          <Text style={styles.contentTitle}>Cacos de Vidro</Text>
+          <Text style={styles.content}>
+            O vidro é um material totalmente...
+          </Text>
+          <Text style={styles.contentTitle}>Dica !</Text>
+          <Text style={styles.content}>
+            Junte os cacos de vidro e coloque-os no meio de uma folha de papel
+            ou jornal que já iria ser descartada. Embrulhe com cuidado para
+            garantir que nada irá cair para fora.
+          </Text>
+          <Text style={styles.contentMessage}>
+            Entre em contato com uma cooperativa que faça a reciclagem do
+            material!
+          </Text>
+          <Text style={styles.contentTip}>Essa dica foi utíl?</Text>
+        </View>
+        <View style={styles.commentContainer}>
+          <Text style={styles.commentTitle}>
+            Têm uma dica para dar? Comente aqui!
+          </Text>
+          <TextInput
+            style={styles.comment}
+            placeholder="Dica"
+            multiline={true}
+          ></TextInput>
+        </View>
       </ScrollView>
     </View>
   );
@@ -16,5 +45,70 @@ export default function DescarteScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 84,
+  },
+  titleConteiner: {
+    backgroundColor: "#142C6C",
+    justifyContent: "center",
+    height: 47,
+    paddingLeft: 24,
+    marginBottom: 25,
+  },
+  title: {
+    fontWeight: "500",
+    color: "#fff",
+    fontSize: 30,
+  },
+  contentContainer: {
+    backgroundColor: "#142C6C",
+    width: 325,
+    marginHorizontal: 35,
+    paddingTop: 40,
+    paddingLeft: 20,
+    paddingBottom: 25,
+    borderRadius: 36,
+    marginBottom: 15,
+  },
+  contentTitle: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  content: {
+    color: "#fff",
+    fontSize: 22,
+    marginBottom: 10,
+    width: 260,
+  },
+  contentMessage: {
+    color: "#fff",
+    fontSize: 22,
+    marginBottom: 10,
+    width: 260,
+    textDecorationLine: "underline",
+  },
+  contentTip: {
+    color: "#fff",
+    fontSize: 22,
+    marginBottom: 10,
+    width: 260,
+  },
+  commentContainer: {
+    backgroundColor: "#C1FFD7",
+    width: 325,
+    minHeight: 110,
+    marginHorizontal: 35,
+    borderRadius: 36,
+    paddingVertical: 15,
+  },
+  commentTitle: {
+    color: "#142C6C",
+    alignSelf: "center",
+    marginBottom: 15,
+  },
+  comment: {
+    width: "80%",
+    marginLeft: 25,
+    color: "#142C6C",
   },
 });
